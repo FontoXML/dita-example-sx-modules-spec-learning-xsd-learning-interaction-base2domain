63299f58cb77e5697634c34858f3c621c7eb46b5
eva <eva.de.haas@liones.nl>
2019-06-28 10:51:54 +0200

    Upgrade to SDK 7.7

--- src/configureSxModule.js
+++ src/configureSxModule.js
@@ -1,25 +1,17 @@
-define([
-	'fontoxml-families/configureAsRemoved',
-	'fontoxml-families/configureAsTitleFrame',
-	'fontoxml-localization/t'
-], function (
-	configureAsRemoved,
-	configureAsTitleFrame,
-	t
-	) {
-	'use strict';
+import configureAsRemoved from 'fontoxml-families/src/configureAsRemoved.js';
+import configureAsTitleFrame from 'fontoxml-families/src/configureAsTitleFrame.js';
+import t from 'fontoxml-localization/src/t.js';
 
-	return function configureSxModule (sxModule) {
-		// lcInteractionBase2
-		configureAsRemoved(sxModule, 'self::lcInteractionBase2', t('lcInteractionBase2'));
+export default function configureSxModule(sxModule) {
+	// lcInteractionBase2
+	configureAsRemoved(sxModule, 'self::lcInteractionBase2', t('lcInteractionBase2'));
 
-		// lcInteractionLabel2
-		configureAsTitleFrame(sxModule, 'self::lcInteractionLabel2', t('interaction label'), {
-			emptyElementPlaceholderText: t('type the interaction label'),
-			fontVariation: 'figure-title'
-		});
+	// lcInteractionLabel2
+	configureAsTitleFrame(sxModule, 'self::lcInteractionLabel2', t('interaction label'), {
+		emptyElementPlaceholderText: t('type the interaction label'),
+		fontVariation: 'figure-title'
+	});
 
-		// lcQuestionBase2
-		configureAsRemoved(sxModule, 'self::lcQuestionBase2', t('lcQuestionBase2'));
-	};
-});
+	// lcQuestionBase2
+	configureAsRemoved(sxModule, 'self::lcQuestionBase2', t('lcQuestionBase2'));
+}
